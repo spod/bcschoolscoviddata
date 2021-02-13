479afd03b6a435f4319a70e9af70e20dfc4323fd
Michael Collins <michael.collins@gmail.com>
2021-02-12 23:45:16 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# bcschoolsdata
-Python experiments with https://bcschoolcovidtracker.knack.com/bc-school-covid-tracker#home/
+# bcschoolscoviddata
+Python experiments with the [BC School Covid Tracker](https://bcschoolcovidtracker.knack.com/bc-school-covid-tracker#home/)
+
+## Quick Start
+* Clone the repo & set up a python virtual env
+* `$ pip install knackpy`
+* `$ python dump_data.py`
+
+## Scripts
+* `dump_data.py` - retrieve all exposure events and save them to `raw_data.json` - note this isn't incremental etc and takes a few seconds to run so don't run it repeatedly
+
+## References
+* [facebook.com/BCSchoolCovidTracker](https://www.facebook.com/BCSchoolCovidTracker)
+* [Knack Developer API Docs](https://docs.knack.com/docs/introduction-to-the-api)
+* [Knackpy](https://github.com/cityofaustin/knackpy) & [Knackpy User Guide](https://cityofaustin.github.io/knackpy/docs/user-guide/)
---
+++ dump_data.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+"""Quick script to dump the raw exposure event data from BC Schools Covid Tracker."""
+
+# https://bcschoolcovidtracker.knack.com/bc-school-covid-tracker#home/
+# https://www.facebook.com/BCSchoolCovidTracker
+
+import json
+import sys
+
+# pip install knackpy
+try:
+    import knackpy
+except ImportError:
+    print("Please install knackpy, see https://cityofaustin.github.io/knackpy/docs/user-guide/#installation")
+    sys.exit(1)
+
+# extracted from web app
+APP_ID='5faae3b10442ac00165da195'
+# extracted from firefox debugger by setting a breakpoint for xhr events
+API_KEY='renderer'
+
+# fetch events data from covid tracker knack app
+app = knackpy.App(app_id=APP_ID, api_key=API_KEY)
+
+print("Fetching all events, this will take a few seconds ...")
+events_records = app.get('Covid Events')
+
+# build a list of raw knack json for all covid event records
+raw = []
+for e in events_records:
+    raw.append(e.raw)
+
+print("Saving to 'raw_data.json' ...")
+with open('raw_data.json', 'w') as f:
+    json.dump(raw, f, indent=2, separators=(',', ': '))
+
+print("Done!")
