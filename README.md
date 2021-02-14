13171242266c7384748fd5b654b2d01caeb5415a
Michael Collins <michael.collins@gmail.com>
2021-02-14 00:24:04 -0800

    json_to_csv.py works!
    Note data is patchy so some csv records may be missing:
    - City
    - Health Region
    - Exposure Dates
    - Extra Info (letter or other source)

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ Python experiments with the [BC School Covid Tracker](https://bcschoolcovidtrack
 * Clone the repo & set up a python virtual env
 * `$ pip install knackpy`
 * `$ python dump_data.py`
+* `$ python json_to_csv.py raw_data.json raw_data.csv`
+* Explore `raw_data.csv` ...
 
 ## Scripts
 * `dump_data.py` - retrieve all exposure events and save them to `raw_data.json` - note this isn't incremental etc and takes a few seconds to run so don't run it repeatedly or even more than daily!
 
-* `json_to_csv.py` - incomplete, script to take output from `dump_data.py` and generate a .csv file for import to excel or other analysis
+* `json_to_csv.py` - script which parses json output from `dump_data.py` and generate a .csv file for import to excel or other analysis tools
 
 * `knack_exp.py` - quick start for exploring bc schools covid data in a python repl, run as `python -i knack_exp.py`, exports the following globals:
   * `app` - a knackpy.App instance for the Covid Tracker Knack app, sample `app.info()`, see Knackpy docs below for more
--- json_to_csv.py
+++ json_to_csv.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
+import argparse
 import html.parser
 import json
+import sys
 
 """Generate csv from json output of dump_data.py"""
 
@@ -26,6 +28,43 @@ raw json data returned from knack.
 >>> r.field_defs
 [<FieldDef 'id'>, <FieldDef 'Address'>, <FieldDef 'City '>, ...]
 """
+
+
+def main():
+    """CLI go brrrrr."""
+    shtmlparser = SimpleHTMLDataParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "injson", nargs="?", type=argparse.FileType("r"), default=sys.stdin
+    )
+    parser.add_argument(
+        "outcsv", nargs="?", type=argparse.FileType("w"), default=sys.stdout
+    )
+    config = parser.parse_args()
+
+    raw_data = json.load(config.injson)
+
+    # write header
+    config.outcsv.write(
+        ", ".join(
+            [
+                "Notification Date",
+                "School",
+                "City",
+                "School District",
+                "Health Region",
+                "Exposure Dates",
+                "Extra Info",
+            ]
+        )
+    )
+    config.outcsv.write("\n")
+
+    # parse and write out each record
+    for record in raw_data:
+        pr = CovidEventRecord(record, shtmlparser)
+        config.outcsv.write(str(pr))
+        config.outcsv.write("\n")
 
 
 class SimpleHTMLDataParser(html.parser.HTMLParser, object):
@@ -62,19 +101,33 @@ class CovidEventRecord:
             "unix_timestamp"
         )
         self.school = self.data.get("field_13_raw", [{}])[0].get("identifier")
-        self.city = self.data.get("field_7_raw", [{}])[0].get("identifier")
+        # some records have no city ...
+        self.city = ""
+        if "field_7_raw" in self.data and self.data["field_7_raw"] is not None:
+            self.city = self.data.get("field_7_raw", [{}])[0].get("identifier")
         self.school_district = self.data.get("field_18_raw", [{}])[0].get("identifier")
-        self.health_region = self.data.get("field_26_raw", [{}])[0].get("identifier")
+        # some records have no health region ... !
+        self.health_region = ""
+        if "field_26_raw" in self.data and self.data["field_26_raw"] is not None:
+            self.health_region = self.data.get("field_26_raw", [{}])[0].get(
+                "identifier"
+            )
         # Both exposure dates and extra info have raw html like: <p>Feb 10, 11</p><p><br /></p>
         # We parse the data out with shtmlparser and if the data contains commas we " quote it
-        shtmlparser.feed(self.data.get("field_15_raw", ""))
-        self.exposure_dates = shtmlparser.get_data()
-        if "," in self.exposure_dates:
-            self.exposure_dates = f'"{self.exposure_dates}"'
-        shtmlparser.feed(self.data.get("field_25_raw", ""))
-        self.extra_info = shtmlparser.get_data()
-        if "," in self.extra_info:
-            self.extra_info = f'"{self.extra_info}"'
+        # some records have no exposure_dates ...
+        self.exposure_dates = ""
+        if "field_15_raw" in self.data and self.data["field_15_raw"] is not None:
+            shtmlparser.feed(self.data.get("field_15_raw", ""))
+            self.exposure_dates = shtmlparser.get_data()
+            if "," in self.exposure_dates:
+                self.exposure_dates = f'"{self.exposure_dates}"'
+        # some records have no extra info ...
+        self.extra_info = ""
+        if "field_25_raw" in self.data and self.data["field_25_raw"] is not None:
+            shtmlparser.feed(self.data.get("field_25_raw", ""))
+            self.extra_info = shtmlparser.get_data()
+            if "," in self.extra_info:
+                self.extra_info = f'"{self.extra_info}"'
 
     def __str__(self):
         return ", ".join(
@@ -106,6 +159,14 @@ def test():
     tr = CovidEventRecord(test_record, parser)
     assert str(tr) == expected
 
+    # test with partial record
+    test_partial = None
+    with open("test2.json", "r") as tf2:
+        test_partial = json.load(tf2)
+    tr2 = CovidEventRecord(test_partial, parser)
+    print(tr2)
+
 
 if __name__ == "__main__":
-    test()
+    # test()
+    main()
